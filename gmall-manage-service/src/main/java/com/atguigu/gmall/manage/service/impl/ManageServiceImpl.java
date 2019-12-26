c62d6400602e3dae60b45c0dccb1a60478788fcf
Nariko <1074618044@qq.com>
2019-12-26 09:50:20 +0800

    属性

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/ManageServiceImpl.java
@@ -0,0 +1,98 @@
+package com.atguigu.gmall.manage.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.*;
+import com.atguigu.gmall.manage.mapper.*;
+import com.atguigu.gmall.service.ManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Service
+public class ManageServiceImpl implements ManageService {
+
+    @Autowired
+    private BaseCatalog1Mapper baseCatalog1Mapper;
+
+    @Autowired
+    private BaseCatalog2Mapper baseCatalog2Mapper;
+
+    @Autowired
+    private BaseCatalog3Mapper baseCatalog3Mapper;
+
+    @Autowired
+    private BaseAttrInfoMapper baseAttrInfoMapper;
+
+    @Autowired
+    private BaseAttrValueMapper baseAttrValueMapper;
+
+    @Autowired
+    private SpuInfoMapper spuInfoMapper;
+
+    @Override
+    public List<BaseCatalog1> getCatalog1() {
+        return baseCatalog1Mapper.selectAll();
+    }
+
+    @Override
+    public List<BaseCatalog2> getCatalog2(String catalog1Id) {
+        BaseCatalog2 baseCatalog2 = new BaseCatalog2();
+        baseCatalog2.setCatalog1Id(catalog1Id);
+        return baseCatalog2Mapper.select(baseCatalog2);
+    }
+
+    @Override
+    public List<BaseCatalog3> getCatalog3(String catalog2Id) {
+        BaseCatalog3 baseCatalog3 = new BaseCatalog3();
+        baseCatalog3.setCatalog2Id(catalog2Id);
+        return baseCatalog3Mapper.select(baseCatalog3);
+    }
+
+    @Override
+    public List<BaseAttrInfo> getAttrList(String catalog3Id) {
+        BaseAttrInfo baseAttrInfo = new BaseAttrInfo();
+        baseAttrInfo.setCatalog3Id(catalog3Id);
+        return baseAttrInfoMapper.select(baseAttrInfo);
+    }
+
+    @Override
+    public void saveAttrInfo(BaseAttrInfo baseAttrInfo) {
+        if (baseAttrInfo.getId() != null && baseAttrInfo.getId().length() > 0){
+            baseAttrInfoMapper.updateByPrimaryKey(baseAttrInfo);
+        } else {
+            baseAttrInfo.setId(null);
+            baseAttrInfoMapper.insertSelective(baseAttrInfo);
+        }
+
+        BaseAttrValue baseAttrValue4Del = new BaseAttrValue();
+        baseAttrValue4Del.setAttrId(baseAttrInfo.getId());
+        baseAttrValueMapper.delete(baseAttrValue4Del);
+
+        if (baseAttrInfo.getAttrValueList() != null && baseAttrInfo.getAttrValueList().size() > 0){
+            for (BaseAttrValue attrValue : baseAttrInfo.getAttrValueList()){
+                attrValue.setId(null);
+                attrValue.setAttrId(baseAttrInfo.getId());
+                baseAttrValueMapper.insertSelective(attrValue);
+            }
+        }
+
+    }
+
+    @Override
+    public BaseAttrInfo getAttrInfo(String attrId) {
+        BaseAttrInfo attrInfo = baseAttrInfoMapper.selectByPrimaryKey(attrId);
+        BaseAttrValue baseAttrValue = new BaseAttrValue();
+        baseAttrValue.setAttrId(attrInfo.getId());
+        List<BaseAttrValue> attrValueList = baseAttrValueMapper.select(baseAttrValue);
+        attrInfo.setAttrValueList(attrValueList);
+        return attrInfo;
+    }
+
+    @Override
+    public List<SpuInfo> getSpuInfoList(SpuInfo spuInfo) {
+        return spuInfoMapper.select(spuInfo);
+    }
+
+}
---
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/ManageController.java
@@ -0,0 +1,55 @@
+package com.atguigu.gmall.manage.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.*;
+import com.atguigu.gmall.service.ManageService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class ManageController {
+
+    @Reference
+    private ManageService manageService;
+
+    @RequestMapping("getCatalog1")
+    public List<BaseCatalog1> getCatalog1(){
+        return manageService.getCatalog1();
+    }
+
+    @RequestMapping("getCatalog2")
+    public List<BaseCatalog2> getCatalog2(String catalog1Id){
+        return manageService.getCatalog2(catalog1Id);
+    }
+
+    @RequestMapping("getCatalog3")
+    public List<BaseCatalog3> getCatalog3(String catalog2Id){
+        return manageService.getCatalog3(catalog2Id);
+    }
+
+    @RequestMapping("attrInfoList")
+    public List<BaseAttrInfo> attrInfoList(String catalog3Id){
+        return manageService.getAttrList(catalog3Id);
+    }
+
+    @RequestMapping("saveAttrInfo")
+    public void saveAttrInfo(@RequestBody BaseAttrInfo baseAttrInfo){
+        manageService.saveAttrInfo(baseAttrInfo);
+    }
+
+    @PostMapping("getAttrValueList")
+    public List<BaseAttrValue> getAttrValueList(String attrId){
+        BaseAttrInfo attrInfo = manageService.getAttrInfo(attrId);
+        return attrInfo.getAttrValueList();
+    }
+
+    @RequestMapping("supList")
+    public List<SpuInfo> supList(String catalog3Id){
+        SpuInfo spuInfo = new SpuInfo();
+        spuInfo.setCatalog3Id(catalog3Id);
+        return manageService.getSpuInfoList(spuInfo);
+    }
+
+}
