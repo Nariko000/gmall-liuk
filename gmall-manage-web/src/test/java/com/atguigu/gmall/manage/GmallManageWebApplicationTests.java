d9f1a1128ef25e4102dec4ae8077bf638b37dc23
Nariko <1074618044@qq.com>
2019-12-28 21:27:40 +0800

    解决日志冲突

--- gmall-manage-web/src/test/java/com/atguigu/gmall/manage/GmallManageWebApplicationTests.java
+++ gmall-manage-web/src/test/java/com/atguigu/gmall/manage/GmallManageWebApplicationTests.java
@@ -29,7 +29,7 @@ public class GmallManageWebApplicationTests {
 		TrackerClient trackerClient=new TrackerClient();
 		TrackerServer trackerServer=trackerClient.getConnection();
 		StorageClient storageClient=new StorageClient(trackerServer,null);
-		String orginalFilename="e://img//zly.jpg";
+		String orginalFilename="C://Users//Nariko//Pictures//Rests//221122.jpg";
 		String[] upload_file = storageClient.upload_file(orginalFilename, "jpg", null);
 		for (int i = 0; i < upload_file.length; i++) {
 			String s = upload_file[i];
